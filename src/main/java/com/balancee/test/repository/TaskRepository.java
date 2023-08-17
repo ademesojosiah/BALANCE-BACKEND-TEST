97f5f28b83fd93f3199c23170d1c942d58230e28
ademesojosiah <josiahademeso@gmail.com>
2023-08-17 23:02:16 +0100

    feat: add custom query to task and user repository

--- src/main/java/com/balancee/test/repository/TaskRepository.java
+++ src/main/java/com/balancee/test/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package com.balancee.test.repository;
 
 import com.balancee.test.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +10,21 @@ import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    public List<Task> findByCompletedTrue();
-    public List<Task> findByCompletedFalse();
+    @Query(value = "SELECT * FROM TASK_TABLE s where s.USER_ID = ?1",
+            nativeQuery = true
+    )
+    List<Task> GetTasksByUserId(Long userId);
+
+    @Query(value = "SELECT * FROM TASK_TABLE s where s.USER_ID = ?1 and s.COMPLETED = TRUE",
+                nativeQuery = true
+    )
+    List<Task> GetTasksByUserIdAndCompletedTrue(Long userId);
+
+    @Query(value = "SELECT * FROM TASK_TABLE s where s.USER_ID = ?1 and s.COMPLETED = FALSE",
+            nativeQuery = true
+    )
+    List<Task> GetTasksByUserIdAndCompletedFalse(Long userId);
+
 
 
 
